afbd49a01d7dc8f485c9ca464eaa175fa6460880
Paul Ecoffet <ecoffet.paul@gmail.com>
2016-10-23 15:29:02 +0200

    clean up

--- csynthesizer/alphabeta2dat.c
+++ csynthesizer/alphabeta2dat.c
@@ -6,27 +6,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int countlines(char *filename) {
-  char ch;
-  int nb_lines = 0;
-  FILE *fp = fopen(filename, "r");
-  while ((ch = fgetc(fp)) != EOF) {
-    if (ch == '\n') {
-      nb_lines++;
-    }
-  }
-  fclose(fp);
-  return nb_lines;
-}
+#include "rk4.h"
+#include "finch.h"
+
 
 int main(int argc, char *argv[]) {
-  int size = 0;
-  if (argc < 3) {
-    fprintf(stderr, "Please provide the salida file name and the envelope file name.\n");
-    return 1;
-  }
-  size = countlines(argv[1]);
-  finch(size, argv[2], argv[1]);
-
+  finch();
   return 0;
 }
