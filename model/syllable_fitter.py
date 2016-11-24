a1d2221d5fcceaba8521f7751c0db70549999630
Paul Ecoffet <ecoffet.paul@gmail.com>
2016-11-24 18:01:57 +0100

    use DTW, update prior and MFCC windows

--- model/syllable_fitter.py
+++ model/syllable_fitter.py
@@ -2,44 +2,70 @@ import numpy as np
 from synth import synthesize, gen_sound, dat2wav
 from python_speech_features import mfcc
 from hill_climbing import hill_climbing
+from fastdtw import fastdtw
+
 
 def fit_syllable(syllable, samplerate=44100):
     best_score = np.inf
     best = None
-    goal = mfcc(syllable, samplerate=samplerate, numcep=8)[..., 1:]
+    goal = mfcc(syllable, samplerate=samplerate,  winlen=0.01, winstep=0.003, numcep=13)[..., 1:]
     #
     i = 1
     j = 3
     prior = []
     dev = []
     for k in range(1,i+1):  # prior on exponential
-        prior.extend([1/k, 1/k])
+        prior.extend([0.5/k, 10/k])
         dev.extend([0.05, 0.05])
     for k in range(1,j+1):
-        prior.extend([10/k, 0, 50*k**2])
-        dev.extend([0.1, 0.05, 5*k])
-    prior.append(0)
-    dev.append(0.05)
-    prior.extend([0, 1, 100, -1]) # beta prior
-    dev.extend([0.005, 0.05, 5, 0.05])
+        prior.extend([0.3/k, 0, 20*k**2])
+        dev.extend([0.001, 0.005, 5*k])
+    prior.append(0.3)
+    dev.append(0.005)
+    prior.extend([0, 0, 0, 1, 100, -0.010]) # beta prior
+    dev.extend([0.005, 0.005, 0.005, 0.005, 5, 0.001])
     x, y, score = hill_climbing(
         lambda x: mfcc(gen_sound(x, len(syllable)),
-                       samplerate, numcep=8)[..., 1:],
+                       samplerate, winlen=0.01, winstep=0.003, numcep=13)[..., 1:],
         goal, np.array(prior), guess_deviation=np.diag(dev),
-        max_iter=20000, verbose=True)
-    print(x)
-    if score < best_score:
-        best_score = score
-        best = x
-    print(score)
-    print(best)
+        max_iter=500, comparison_method=lambda g, c: fastdtw(g, c, dist=2)[0],
+        temp_max=10,
+        verbose=False)
 
-    return best
+
+    return x, score
 
 
 if __name__ == "__main__":
     from scipy.io import wavfile
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from synth import f_str
+    from synth import gen_alphabeta
     sr, tutor_syllable = wavfile.read('../data/ba_syllable_a.wav')
-    x = fit_syllable(tutor_syllable, samplerate=sr)
+    plt.plot(tutor_syllable)
+    plt.show()
+    best_x = None
+    best_score = np.inf
+    for i in range(20):
+        try:
+            x, score = fit_syllable(tutor_syllable, samplerate=sr)
 
-    wavfile.write('out.wav', sr, dat2wav(gen_sound(x, len(tutor_syllable))))
+            plt.plot(gen_sound(x, len(tutor_syllable)))
+            plt.show()
+            print('alpha(t) = ' + f_str(x[:1*2+3*3+1], nb_exp=1, nb_sin=3, x='t'))
+            print('beta(t) = ' + f_str(x[1*2+3*3+1:], nb_exp=1, nb_sin=1, x='t'))
+            plt.plot(np.arange(0,len(tutor_syllable)+2)/44100, gen_alphabeta(x, len(tutor_syllable))[:, 0], label='a')
+            plt.plot(np.arange(0,len(tutor_syllable)+2)/44100, gen_alphabeta(x, len(tutor_syllable))[:, 1], label='b')
+            plt.legend()
+            plt.show()
+            if score < best_score:
+                best_x = x
+                best_score = score
+            print('{}: {} (best: {})'.format(i, score, best_score))
+        except KeyboardInterrupt:
+            break
+
+    print(best_x)
+    print('score', best_score)
+    wavfile.write('out.wav', sr, dat2wav(gen_sound(best_x, len(tutor_syllable))))
--- model/synth.py
+++ model/synth.py
@@ -4,19 +4,32 @@ from io import BytesIO
 
 def f(x, p, nb_exp=2, nb_sin=2):
     ip = np.nditer(p)
-    x = x/22500
     return np.sum([next(ip) * np.exp(-next(ip) * x) for i in range(nb_exp)]
-                + [next(ip) * np.sin((next(ip) + 2*np.pi * x) / next(ip)) for i in range(nb_sin)] + [next(ip)], axis=0)
+                + [next(ip) * np.sin((next(ip) + 2*np.pi * x) * next(ip)) for i in range(nb_sin)] + [next(ip)], axis=0)
 
-def gen_sound(params, length, alphaf_shape=(1, 3), betaf_shape=(0, 1)):
-    t = np.arange(0, length+2)  # + 2 padding is necessary with ba synth.
+def f_str(p, nb_exp=2, nb_sin=2, x='x'):
+    ip = np.nditer(p)
+    out = ''
+    for i in range(nb_exp):
+        out += '{:.2} exp({:.2} {}) + '.format(float(next(ip)), float(next(ip)), x)
+    for i in range(nb_sin):
+        out += '{:.2f} sin(({:.2f} + 2π {})/{:.2f}) + '.format(float(next(ip)), float(next(ip)), x, float(next(ip)))
+    out += '{:.2f}'.format(float(next(ip)))
+    return out
+
+def gen_sound(params, length, alphaf_shape=(1, 3), betaf_shape=(1, 1)):
+    alpha_beta = gen_alphabeta(params, length, alphaf_shape, betaf_shape)
+    out = synthesize(alpha_beta)
+    return out
+
+def gen_alphabeta(params, length, alphaf_shape=(1, 3), betaf_shape=(1, 1)):
+    t = np.linspace(0, (length+2)/44100, length+2)  # + 2 padding is necessary with ba synth.
     nb_args_alpha = alphaf_shape[0] * 2 + alphaf_shape[1] * 3 + 1
     alpha_beta = np.stack((
         f(t, params[:nb_args_alpha], alphaf_shape[0], alphaf_shape[1]),
         f(t, params[nb_args_alpha:], betaf_shape[0], betaf_shape[1])), axis=-1
     )
-    out = synthesize(alpha_beta)
-    return out
+    return alpha_beta
 
 def synthesize(alpha_beta, amplitudeforwav=False):
     input_bytes = BytesIO()
