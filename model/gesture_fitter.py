490563de63daa1f06b23a6b6cd3648f64dfa275b
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-01-30 17:22:15 +0100

    slightly better priors

--- model/gesture_fitter.py
+++ model/gesture_fitter.py
@@ -16,12 +16,12 @@ def fit_gesture(gesture, samplerate=44100):
     mins = []
     maxs = []
     for k in range(1,i+1):  # prior on exponential
-        prior.extend([0.5/k, 10/k])
+        prior.extend([1/k, 10/k])
         dev.extend([0.05, 0.05])
         mins.extend([-10, 0])
         maxs.extend([10, 10])
     for k in range(1,j+1):
-        prior.extend([1/k, 0, 20*k**2])
+        prior.extend([0.1/k, 0, 200*k**2])
         dev.extend([0.001, 0.005, 5*k])
         mins.extend([0, -np.pi, 0])
         maxs.extend([10, np.pi, 2000])
@@ -35,14 +35,12 @@ def fit_gesture(gesture, samplerate=44100):
     mins.extend([-10, 0, 0, -np.pi, 0, -3])
     maxs.extend([10, 10, 3, np.pi, 1000, 0])
     x, y, score = hill_climbing(
-        lambda x: mfcc(gen_sound(x, len(syllable), alphaf_shape=(i, j)),
+        lambda x: mfcc(gen_sound(x, len(gesture), alphaf_shape=(i, j)),
                        samplerate,winstep=0.005, numcep=26, nfilt=52, nfft=1024)[..., 1:],
         goal, np.array(prior), guess_min=mins, guess_max=maxs, guess_deviation=np.diag(dev),
-        max_iter=500, comparison_method=lambda g, c: fastdtw(g, c, dist=2)[0],
+        max_iter=1000, comparison_method=lambda g, c: fastdtw(g, c, dist=2)[0],
         temp_max=10,
         verbose=False)
-
-
     return x, score
 
 
@@ -54,27 +52,27 @@ if __name__ == "__main__":
     import seaborn as sns
     from synth import f_str
     from synth import gen_alphabeta
-    sr, tutor_syllable = wavfile.read('../data/ba_syllable_b.wav')
-    plt.plot(tutor_syllable)
-    plt.show()
+    sr, tutor_syllable = wavfile.read('../data/ba_syllable_a_end.wav')
     best_x = None
     best_score = np.inf
     run_name = datetime.datetime.now().strftime('%y%m%d_%H%M')
     os.makedirs('res/{}/'.format(run_name), exist_ok=True)
     for i in range(20):
         try:
-            x, score = fit_syllable(tutor_syllable, samplerate=sr)
+            x, score = fit_gesture(tutor_syllable, samplerate=sr)
             print("*"*80)
+            plt.figure()
             plt.plot(gen_sound(x, len(tutor_syllable)))
-            plt.show()
+            plt.savefig('res/{}_song.svg'.format(i))
             print(x)
             print('alpha(t) = ' + f_str(x[:1*2+3*3+1], nb_exp=1, nb_sin=3, x='t'))
             print('beta(t) = ' + f_str(x[1*2+3*3+1:], nb_exp=1, nb_sin=1, x='t'))
             alpha_beta = gen_alphabeta(x, len(tutor_syllable))
+            plt.figure()
             plt.plot(np.arange(0,len(tutor_syllable)+2)/44100, alpha_beta[:, 0], label='a')
             plt.plot(np.arange(0,len(tutor_syllable)+2)/44100, alpha_beta[:, 1], label='b')
             plt.legend()
-            plt.show()
+            plt.savefig('res/{}_param.svg'.format(i))
             if score < best_score:
                 best_x = x
                 best_score = score
