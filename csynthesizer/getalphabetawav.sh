46aea99833466728625be47e437297272dae3b90
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-02-23 17:54:44 +0100

    Update synth_similarity, try to fit gesture without success

--- csynthesizer/getalphabetawav.sh
+++ csynthesizer/getalphabetawav.sh
@@ -13,8 +13,12 @@ echo "converting to ascii wav"
 ./wta gabwtmp.wav > gabwtmp.dat
 
 # Prepend with silence because otherwise the synth ignore the very first
-# sounds.
-cat silence.dat gabwtmp.dat > gabwtmp_prep.dat
+# and very last sounds.
+cat silence.dat gabwtmp.dat silence.dat > gabwtmp_prep.dat
+
+refsonglength=$(cat gabwtmp.dat | wc -l)
+
+echo "Length of the first signal $refsonglength"
 
 echo "getting fundamental frequencies"
 nbinfo=$(cat gabwtmp_prep.dat | wc -l)
@@ -30,20 +34,40 @@ import numpy as np
 alpha = np.loadtxt('env_gabwtmp_prep.dat')[:, 2]
 beta = np.loadtxt('gabwtmp_out.dat')[:, 1]
 out = np.stack((alpha[:beta.shape[0]], beta), axis=-1)
-np.savetxt('gabwtmp_ab.dat', out[1000:])
+np.savetxt('gabwtmp_ab.dat', out[1000:-1000])
 
 EOF
 echo $(cat gabwtmp_ab.dat | wc -l) | cat - gabwtmp_ab.dat > gabwtmp_ab_num.dat
 
-# Now, let's remove the 1000 zeros we have put in the wave
+# Now, let's remove the zeros we have put in front and at the end of the wave
+# We take the data so it's the exact same length
 
-tail -n +1001 song_gabwtmp_out.dat > song_gabwtmp_out_t.dat
 
-./dat2wav song_gabwtmp_out_t.dat gabwtmp_out.wav 44100
+tail -n +1001 song_gabwtmp_out.dat | head -n $refsonglength > song_gabwtmp_out_t.dat
+
+echo "Length of out signal"
+echo `cat song_gabwtmp_out_t.dat | wc -l`
+
+echo "Converting to WAV file"
+
+python - << EOF
+from scipy.io import wavfile
+import numpy as np
+
+signal = np.loadtxt('song_gabwtmp_out_t.dat', dtype=float)
+signal -= np.mean(signal)
+signal = 2*(signal - np.min(signal)) / (np.max(signal) - np.min(signal)) - 1
+wavfile.write('gabwtmp_out.wav', 44100, signal)
+
+signal = np.loadtxt('gabwtmp.dat', dtype=float)
+signal -= np.mean(signal)
+signal = 2*(signal - np.min(signal)) / (np.max(signal) - np.min(signal)) - 1
+wavfile.write('gabwtmp.wav', 44100, signal)
+EOF
 
 mv gabwtmp_ab.dat "$first_dir/${fname}_ab.dat"
 mv song_gabwtmp_out_t.dat "$first_dir/${fname}_out.dat"
 mv gabwtmp_ab_num.dat "$first_dir/${fname}_out_num.dat"
 mv gabwtmp_out.wav "$first_dir/${fname}_out.wav"
 
-rm *gabwtmp*
+#rm *gabwtmp*
--- model/hillclimbing/gesture_fitter.py
+++ model/hillclimbing/gesture_fitter.py
@@ -8,7 +8,7 @@ import birdsonganalysis as bsa
 
 
 def _calc_res(sig, sr):
-    fnames = ['am', 'fm', 'pitch', 'goodness', 'entropy']
+    fnames = ['fm', 'pitch', 'goodness', 'entropy']
     features = bsa.normalize_features(
         bsa.all_song_features(sig, sr, without="amplitude"))
     out = []
@@ -20,7 +20,8 @@ def _calc_res(sig, sr):
 def fit_gesture(gesture, samplerate=44100):
     """Find the parameters to fit to a gesture."""
     size = len(gesture)
-    goal = _calc_res(gesture, samplerate)
+    #goal = _calc_res(gesture, samplerate)
+    goal = np.copy(gesture)
     i = 1
     j = 3
     prior = []
@@ -28,15 +29,15 @@ def fit_gesture(gesture, samplerate=44100):
     mins = []
     maxs = []
     for k in range(1, i + 1):  # prior on exponential
-        prior.extend([0.1/k, 10/k])
+        prior.extend([0.01/k, 10/k])
         dev.extend([0.05, 0.05])
         mins.extend([-10, 0])
         maxs.extend([10, 5000])
     for k in range(1, j + 1):  # prior on sin
         prior.extend([0.5/k, 0, 200*k**2])
-        dev.extend([0.05/k, 0.005, 5*k])
+        dev.extend([0.01/k, 0.005, 5*k])
         mins.extend([0, -np.pi, 0])
-        maxs.extend([10, np.pi, 8000])
+        maxs.extend([3, np.pi, 8000])
     prior.append(4)
     mins.append(0)
     maxs.append(10)
@@ -51,15 +52,16 @@ def fit_gesture(gesture, samplerate=44100):
     mins.extend([-10, 0, 0, -np.pi, 0, -3])
     maxs.extend([10, 1000, 3, np.pi, 1000, 0])
     x, y, score = hill_climbing(
-        function=lambda x: _calc_res(gen_sound(x, size), samplerate),
+        # function=lambda x: _calc_res(gen_sound(x, size), samplerate),
+        function=lambda x: gen_sound(x, size),
         goal=goal,
         guess=np.array(prior),
         guess_min=mins,
         guess_max=maxs,
         guess_deviation=np.diag(dev),
-        max_iter=500,
-        comparison_method=lambda g, c: fastdtw(g, c)[0],
-        temp_max=1,
+        max_iter=5000,
+        comparison_method=lambda g, c: np.linalg.norm(g - c),
+        temp_max=100,
         verbose=False)
     return x, score
 
@@ -86,9 +88,12 @@ if __name__ == "__main__":
     true_params = np.loadtxt('../../data/ba_syllable_a_end_ab.dat')
     plt.plot(true_params)
     plt.savefig('res/{}/ref_params.svg'.format(run_name))
-    g = _calc_res(tutor_syllable, sr)
-    c = _calc_res(synth_syllable, sr)
-    score = fastdtw(g, c)[0]
+    #g = _calc_res(tutor_syllable, sr)
+    #c = _calc_res(synth_syllable, sr)
+    g = tutor_syllable
+    c = synth_syllable
+    #score = fastdtw(g, c)[0]
+    score = np.linalg.norm(g - c)
     print('score between real and synth: {}'.format(score))
     for i in range(1):
         try:
