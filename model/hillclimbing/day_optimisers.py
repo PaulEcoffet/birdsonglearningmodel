f25a72059f51fa783acb1f3a6f64e63288139c10
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-03-13 17:13:32 +0100

    less heavy logs

--- model/hillclimbing/day_optimisers.py
+++ model/hillclimbing/day_optimisers.py
@@ -43,7 +43,7 @@ def optimise_gesture_dummy(songs, tutor_song, measure, comp, train_per_day=10,
             tutor_song[start:end].copy(), measure, comp, start_prior=prior,
             nb_iter=nb_iter_per_train, temp=None, rng=rng)
         datasaver.add(pre_score=pre_score,
-                      new_score=hill_score, song=song, isong=isong, ig=ig)
+                      new_score=hill_score, isong=isong, ig=ig)
         songs[isong].gestures[ig][1] = deepcopy(res)
         assert pre_score >= hill_score
     return songs
